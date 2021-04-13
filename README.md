53a07d7f762c69854aca3d140c56827f0a335583
kurvinh <kurvinhendricks@gmail.com>
2021-04-13 22:07:19 +0200

    https://trello.com/c/tUKx98A1 upload form, base JS gets the auth token from sacosbackend

--- README.md
+++ README.md
@@ -7,6 +7,10 @@
 # use GITHUB api https://docs.github.com/en/rest
 # https://github.com/octokit/rest.js
 
+# https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
+# https://stackoverflow.com/questions/6978156/get-base64-encode-file-data-from-input-form
+
+
 # form 2
 # correction form
 # TBD
--- modules/pages/data-upload/src/upload.js
+++ modules/pages/data-upload/src/upload.js
@@ -1,13 +1,60 @@
 import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 
-const octokit = new Octokit();
+let octokit = null
 
-// Compare: https://docs.github.com/en/rest/reference/repos/#list-organization-repositories
-octokit.rest.repos
-  .listForOrg({
-    org: "octokit",
-    type: "public",
-  })
-  .then(({ data }) => {
-    // handle data
-  });
+init(upload)
+
+function init(callback) {
+
+	let apiurl = window.localStorage.getItem('apiurl')
+
+    let xhr = new XMLHttpRequest()
+    let url = apiurl + '/auth/GHUB_SACOS_TOKEN'
+
+    xhr.open("GET", url)
+    xhr.setRequestHeader('Accept', 'application/json; charset=UTF-8')
+    xhr.send()
+
+    xhr.onreadystatechange = (e) => {
+    	if (xhr.readyState === 4) {
+            const token = JSON.parse(xhr.responseText)
+            console.log(token)
+
+            octokit = new Octokit({
+			  auth: token.token
+			});
+            //Wait until data is done being retrieved then run callback
+            callback(octokit)
+        }
+    }	
+}
+
+function upload(octokit) {
+
+	const repocontents = octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+	  owner: 'HendricksK',
+	  repo: 'sacos_images',
+	  path: 'uploads/dataform'
+	})
+
+	console.log(repocontents)
+
+	// Getting 404 error here because of auth.
+	try {
+		// https://octokit.github.io/rest.js/v18#repos-create-or-update-file-contents
+		upload = octokit.rest.repos.createOrUpdateFileContents({
+	        owner: 'HendricksK',
+			repo: 'sacos_images',
+			path: 'uploads/dataform/SSBidXkgbXkgd2VlZCBpbiBjYWxpZm9ybmlh_3.txt',
+			message: 'this is a commit message',
+			content: 'SSBidXkgbXkgd2VlZCBpbiBjYWxpZm9ybmlh',
+			sha: ''
+	      })
+
+		console.log(upload)
+
+	} catch (e) {
+		console.log(e)
+	}
+}	
+
